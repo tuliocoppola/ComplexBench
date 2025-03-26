b7ce84caace6b312bee5058f8348e42edb3841f9
Prince Kumar <prince.kumar12@ibm.com>
2025-03-26 16:48:03 -0400

    un-commented bash commands

--- eval.sh
+++ eval.sh
@@ -16,24 +16,24 @@ python3 evaluation/llm_based_extraction.py \
     --language $language
 
 
-# python3 evaluation/llm_based_evaluation.py \
-#     --data_path $data_path \
-#     --llm_output_path $llm_output_path \
-#     --output_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
-#     --api_key $api_key \
-#     --api_base $api_base \
-#     --language $language
+python3 evaluation/llm_based_evaluation.py \
+    --data_path $data_path \
+    --llm_output_path $llm_output_path \
+    --output_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
+    --api_key $api_key \
+    --api_base $api_base \
+    --language $language
 
 
-# python3 evaluation/rule_based_evaluation.py \
-#     --data_path $data_path \
-#     --extraction_path "${output_dir}/${model_name}_llm_extraction_results.jsonl" \
-#     --output_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl"
+python3 evaluation/rule_based_evaluation.py \
+    --data_path $data_path \
+    --extraction_path "${output_dir}/${model_name}_llm_extraction_results.jsonl" \
+    --output_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl"
 
 
-# python3 evaluation/aggregation.py \
-#     --data_path $data_path \
-#     --llm_evaluation_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
-#     --rule_evaluation_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl" \
-#     --model $model_name \
-#     --output_path $output_dir
+python3 evaluation/aggregation.py \
+    --data_path $data_path \
+    --llm_evaluation_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
+    --rule_evaluation_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl" \
+    --model $model_name \
+    --output_path $output_dir
