2d644242af9eff60741dd62e1e4a780ba75b2bc1
Tulio Coppola <tuliocoppola@ibm.com>
2025-07-03 09:41:20 -0300

    bash improvs

--- eval.sh
+++ eval.sh
@@ -6,11 +6,12 @@ api_base=$5
 model_name=$6
 language=$7
 
+output_string=$(echo "${output_dir}/${model_name//\//_}" | tr -s /)
 
 python3 evaluation/llm_based_extraction.py \
     --data_path $data_path \
     --llm_output_path $llm_output_path \
-    --output_path "${output_dir}/${model_name}_llm_extraction_results.jsonl" \
+    --output_path "${output_string}_llm_extraction_results.jsonl" \
     --api_key $api_key \
     --api_base $api_base \
     --language $language \
@@ -20,7 +21,7 @@ python3 evaluation/llm_based_extraction.py \
 python3 evaluation/llm_based_evaluation.py \
     --data_path $data_path \
     --llm_output_path $llm_output_path \
-    --output_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
+    --output_path "${output_string}_llm_evaluation_results.jsonl" \
     --api_key $api_key \
     --api_base $api_base \
     --language $language \
@@ -29,13 +30,13 @@ python3 evaluation/llm_based_evaluation.py \
 
 python3 evaluation/rule_based_evaluation.py \
     --data_path $data_path \
-    --extraction_path "${output_dir}/${model_name}_llm_extraction_results.jsonl" \
-    --output_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl"
+    --extraction_path "${output_string}_llm_extraction_results.jsonl" \
+    --output_path "${output_string}_rule_evaluation_results.jsonl"
 
 
 python3 evaluation/aggregation.py \
     --data_path $data_path \
-    --llm_evaluation_path "${output_dir}/${model_name}_llm_evaluation_results.jsonl" \
-    --rule_evaluation_path "${output_dir}/${model_name}_rule_evaluation_results.jsonl" \
-    --model $model_name \
+    --llm_evaluation_path "${output_string}_llm_evaluation_results.jsonl" \
+    --rule_evaluation_path "${output_string}_rule_evaluation_results.jsonl" \
+    --model "${model_name//\//_}" \
     --output_path $output_dir
